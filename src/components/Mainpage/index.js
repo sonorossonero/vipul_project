264ee48c5c31b5c7a18ca9d772939a8be4826c18
sonorossonero <vipul.dtu.2k16@gmail.com>
2023-11-19 09:44:28 +0530

    Basic project structure

---
+++ src/components/Mainpage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Mainpage() {
+    return (
+        <>
+            <div className="frame_mainPage">
+                <h1>Main Page</h1>
+            </div>
+        </>
+    )
+}
+
+export default Mainpage
---
+++ src/components/SideNavigation/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function SideNavigation() {
+    return (
+        <>
+        <div className='frame_sideNavigation'>
+            <h1>Side Navigation Panel</h1>
+        </div>
+        
+        </>
+    )
+}
+
+export default SideNavigation
