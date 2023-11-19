ad53dec201d49d39d99d1bb9ee279fd3ddb19785
sonorossonero <vipul.dtu.2k16@gmail.com>
2023-11-19 10:41:36 +0530

    Side navigation panel attached

--- src/components/SideNavigation/index.js
+++ src/components/SideNavigation/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import './style.css';
+import './style.css'; // assuming you have a separate stylesheet
+import userData from '../../assets/userInfo.json';
 
 function SideNavigation() {
     return (
         <>
-        <div className='frame_sideNavigation'>
-            <h1>Side Navigation Panel</h1>
-        </div>
-        
+            <div className='customFrame_sideNavigation'></div>
+            <div className='customPanel'>
+                <div className='customNavPages'>
+                    <h1 className='customLogo'>BRAND NAME</h1>
+                    <ul>
+                        <li>Overview</li>
+                        <li>Onboarding</li>
+                        <li className='customActive'>Monitoring</li>
+                        <li>Flagging</li>
+                        <li>Source of Income</li>
+                        <li>UAR</li>
+                    </ul>
+                </div>
+
+                <div className='customUserInfo'>
+                    <img src={userData.photo} alt="user" />
+                    <div className='customUserInfoContainer'>
+                        <h3>{userData.name}</h3>
+                        <p>{userData.email}</p>
+                    </div>
+                </div>
+            </div>
         </>
-    )
+    );
 }
 
-export default SideNavigation
+export default SideNavigation;
